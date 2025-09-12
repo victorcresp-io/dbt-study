50350659670e67dd9865ddcba5066342cca384cc
Victor Rodrigues <contactvictorcrespo@gmail.com>
2025-09-12 15:10:02 -0300

    teste1

--- src/test_dbt_sqlite.py
+++ src/test_dbt_sqlite.py
@@ -9,13 +9,22 @@ print(path_db)
 conn =  sqlite3.connect(path_db)
 
 cursor = conn.cursor()
-res = cursor.execute("""
+
+'''res = cursor.execute("""
     SELECT *
     FROM sqlite_master
     WHERE type = 'view'
     AND name = 'staging_person'
 """).fetchall()
-
-'''res = cursor.execute("SELECT * FROM staging_person").fetchall()
 '''
+'''res = cursor.execute("SELECT * FROM data_age_dbt_study.csv").fetchall()
+'''
+
+res = cursor.execute("""
+    SELECT name, type 
+    FROM sqlite_master
+    WHERE type IN ('table', 'view');
+""").fetchall()
+
+
 print(res)
